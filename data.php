e20ec314540003ebd8a052137ceffe3f1ebd5f94
jacoobwang <531532957@qq.com>
2016-11-14 22:17:26 -0500

    modify data.php

--- data.php
+++ data.php
@@ -1,12 +1,13 @@
 <?php
 	error_reporting(0);
 	$data = array(
-		'day' => isset($_GET['day']) ? $_GET['day'] : false,
-		'user'=> isset($_GET['user'])? $_GET['user']: false
+		'sDay' => isset($_GET['day']) ? $_GET['day'] : false,
+		'sUser'=> isset($_GET['user'])? $_GET['user']: false
 	);
-	$sql = "insert into daka (".implode(',',array_keys($data)).") values (".implode(',',array_values($data)).")";
-	
-	$conn = mysql_connect('localhost:3306','user','user');
+	$sql = "insert into daka (".implode(',',array_keys($data)).") values ('".implode("','",array_values($data))."')";
+	$conn = mysql_connect('localhost:3306','root','123456');
+	mysql_query("set names 'utf8'");
+	mysql_select_db("daka");
 	$rs = mysql_query($sql,$conn);
 	if($rs != false) echo 1;
 ?>
